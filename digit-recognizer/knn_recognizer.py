6a7e0c6831689226ec7ec3982b0b605e03a5e866
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2012-12-23 07:43:11 +0400

    Run code added

--- digit-recognizer/knn_recognizer.py
+++ digit-recognizer/knn_recognizer.py
@@ -11,3 +11,38 @@ class Recognizer:
 
     def recognize(self, digit):
         return self.__classifier.predict(digit)[0]
+
+def __main():
+    def print_usage(script_name):
+        print "Usage:"
+        print "%s train_data.npy train_labels.npy test_data.npy output_file" % (script_name)
+
+    from sys import argv
+    from numpy import load
+    from os.path import basename
+
+    if len(argv) != 5:
+        print_usage(basename(argv[0]))
+        exit(1)
+
+    train_data = load(argv[1])
+    train_labels = load(argv[2])
+    test_data = load(argv[3])
+    output_file = argv[4]
+
+    recognizer = Recognizer(train_data, train_labels)
+
+    results = []
+    total_examples = len(test_data)
+    for i, test_example in enumerate(test_data):
+        predicted = recognizer.recognize(test_example)
+        print 'Example %d/%d\tpredicted %d' % (i, total_examples, predicted)
+        results.append(predicted)
+
+    with open(output_file, 'w') as f:
+        for r in results:
+            f.write(str(r) + '\n')
+
+# ---
+if __name__ == "__main__":
+    __main()
